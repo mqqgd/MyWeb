41b05d0d0cf8465c0c90fd11d532b9f6c729532b
mqqgd <3216089290@qq.com>
2021-03-03 22:19:00 +0800

    the forth closure

---
+++ JS/关系运算.js
@@ -0,0 +1,4 @@
+var a = 1 > "10";
+console.log(a);
+let v = 1 > "10";
+console.log(v);
---
+++ JS/闭包练习.js
@@ -0,0 +1,43 @@
+// function test() {
+//     var tempt = 100;
+//     function a() {
+//         console.log(tempt)
+//     }
+//     return a;
+// }
+
+// var demo = test();
+// demo();
+
+
+// function a() {
+//     function b() {
+//         var bbb = 233;
+//         document.write(aaa);
+//     }
+
+//     var aaa = 123;
+//     return b;
+// }
+// var glob = 100;
+// var demo = a();
+// demo();    //demo执行b执行，demo其实就是b
+
+
+//闭包
+function test() {
+    var arr = [];
+    for (var i = 0; i < 10; i++) {
+        arr[i] = function () {
+            console.log(i);
+
+        }
+    }
+    return arr;
+}
+
+var myArr = test();
+// myArr[0]();
+for (var j = 0; j < 10; j++) {
+    myArr[j]();  //会打出10个10
+}
