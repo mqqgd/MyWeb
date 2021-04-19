24181500ba6b10facc88104b0cb6b9084e2a9cba
mqqgd <3216089290@qq.com>
2021-04-19 17:29:02 +0800

    binary tree traversal

---
+++ Algorithms/左神/二叉树中序遍历.js
@@ -0,0 +1,81 @@
+/* 思路：当前节点为空，从栈拿一个打印，当前节点往右跑
+     当前节点不为空，当前节点压入栈，当前节点往左 */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+// var TreeNode = {
+//     val: 1,
+//     left: {
+//         val: 2,
+//         left: {
+//             val: 4,
+//         },
+//         right: {
+//             val: 5
+//         }
+//     },
+//     right: {
+//         val: 3,
+//         left: {
+//             val: 6,
+//         },
+//         right: {
+//             val: 7
+//         }
+//     }
+// };
+
+function inorderTraversal(root) {
+    // 牛客
+    // var inorderTraversal = function(root) {
+    let stackArr = [];
+    let res = [];
+    if (root != undefined) {
+        while (stackArr != 0 || root != undefined) {
+            if (root != undefined) {
+                stackArr.push(root);
+                root = root.left;
+            } else {
+                root = stackArr.pop();
+                // console.log(root.val + "");
+                res.push(root.val);
+                root = root.right;
+            }
+        }
+    }
+    return res;
+
+};
+
+// var res = inorderTraversal(TreeNode);
+// console.log(res)
+
+// 定义树节点
+function TreeNode(val) {
+    // this.val = (val === undefined ? 0 : val)
+    // this.left = (left === undefined ? null : left)
+    // this.right = (right === undefined ? null : right)
+    this.val = val;
+    this.left = null;
+    this.right = null;
+}
+
+// 构建树
+let root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+var inor = inorderTraversal(root);
+console.log(inor)
---
+++ Algorithms/左神/二叉树先序遍历.js
@@ -0,0 +1,82 @@
+// 问题：用递归和非递归的方式实现二叉树的先序遍历
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+// 非递归方法
+function preorderTraversal(root) {
+    // var preorderTraversal = function(root) {
+    let stackArr = [];
+    let res = [];
+    if (root != undefined) {
+        stackArr.push(root);
+
+        while (stackArr.length != 0) {
+            root = stackArr.pop();
+            // console.log(root.val + "");
+            res.push(root.val)
+
+            //不可以写成root.right==null ,否则报错：TypeError: Cannot read property 'val' of undefined
+            if (root.right != undefined) {
+                stackArr.push(root.right);
+            }
+            if (root.left != undefined) {
+                stackArr.push(root.left);
+            }
+        }
+    }
+    return res;
+};
+
+// var TreeNode = {
+//     val: 1,
+//     left: {
+//         val: 2,
+//         left: {
+//             val: 4,
+//         },
+//         right: {
+//             val: 5
+//         }
+//     },
+//     right: {
+//         val: 3,
+//         left: {
+//             val: 6,
+//         },
+//         right: {
+//             val: 7
+//         }
+//     }
+// };
+// var pre = preorderTraversal(TreeNode);
+// console.log(pre)
+
+
+
+
+// 树节点定义
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+// 构建树
+let root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+var pre = preorderTraversal(root);
+console.log(pre)
