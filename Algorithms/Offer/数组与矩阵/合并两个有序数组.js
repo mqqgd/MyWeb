a0ad79cef4f42d8c9879ea77e22051ef78d2c07c
mqqgd <3216089290@qq.com>
2021-06-29 16:47:10 +0800

    update

--- Algorithms/Offer/数组与矩阵/合并两个有序数组.js
+++ Algorithms/Offer/数组与矩阵/合并两个有序数组.js
@@ -1,20 +1,38 @@
-function mergeArr(A, B) {
-    let help = [];
-    let i = 0,
-        j = 0,
-        k = 0;
-    while (i < A.length && j < B.length) {
-        help[k++] = (A[i] <= B[j]) ? A[i++] : B[j++];
+// function mergeArr(A, B) {
+//     let help = [];
+//     let i = 0,
+//         j = 0,
+//         k = 0;
+//     while (i < A.length && j < B.length) {
+//         help[k++] = (A[i] <= B[j]) ? A[i++] : B[j++];
+//     }
+//     while (i < A.length) {
+//         help[k++] = A[i++];
+//     }
+//     while (j < B.length) {
+//         help[k++] = B[j++];
+//     }
+//     return help;
+// }
+// let A = [];
+// let B = [1];
+// let res = mergeArr(A, B)
+// console.log(res);
+
+
+var merge = function(nums1, m, nums2, n) {
+    let i = m - 1,
+        j = n - 1,
+        k = m + n - 1;
+
+    while (i >= 0 && j >= 0) {
+        nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
     }
-    while (i < A.length) {
-        help[k++] = A[i++];
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
     }
-    while (j < B.length) {
-        help[k++] = B[j++];
-    }
-    return help;
-}
-let A = [];
-let B = [1];
-let res = mergeArr(A, B)
-console.log(res);
+    return nums1;
+
+
+
+};
